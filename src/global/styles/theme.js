b8b121daf6ade07e360864d2b266f814dec9703d
@lucas.vuicik <lucas.fernandes@paipe.co>
2023-01-05 15:31:08 -0300

    feat: created font and bold tokens

--- src/global/styles/theme.js
+++ src/global/styles/theme.js
@@ -1,4 +1,5 @@
 import { DefaultTheme } from "react-native-paper";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const colors = {
   black: "#000",
@@ -34,5 +35,21 @@ export const theme = {
     blue600: "#154c79",
     orange: "#ff7143",
     red: "#E83F5B",
+  },
+
+  fontSize: {
+    verySmall: RFValue(10),
+    small: RFValue(12),
+    medium: RFValue(14),
+    extraMedium: RFValue(18),
+    big: RFValue(20),
+    extraBig: RFValue(32),
+  },
+
+  bold: {
+    small: "200",
+    extraSmall: "400",
+    medium: "600",
+    bolder: "900",
   },
 };
