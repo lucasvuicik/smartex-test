7e4fab86df9a5f4b86059596283151acaca861a3
@lucas.vuicik <lucas.fernandes@paipe.co>
2023-01-05 12:06:03 -0300

    feat: added a theme provider to the project (to use colors and tokens)

--- App.tsx
+++ App.tsx
@@ -4,12 +4,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Routes } from "./src/routes";
 import { StatusBar } from "react-native";
+import { theme } from "./src/global/styles/theme";
+
 
 export const App = () => {
     return (
         <NavigationContainer>
             <StatusBar hidden={true} />
-            <PaperProvider>
+            <PaperProvider theme={theme}>
                 <Routes />
             </PaperProvider>
         </NavigationContainer>
