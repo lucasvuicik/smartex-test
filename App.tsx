5d596d56600f841968a255b7633ad9a032f5a199
@lucas.vuicik <lucas.fernandes@paipe.co>
2023-01-05 20:01:32 -0300

    feat: added user provider

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider as PaperProvider } from 'react-native-paper';
+
+import { UserProvider } from "./src/contexts/UserContext";
+
 import { Routes } from "./src/routes";
 import { StatusBar } from "react-native";
 import { theme } from "./src/global/styles/theme";
@@ -9,12 +12,14 @@ import { theme } from "./src/global/styles/theme";
 
 export const App = () => {
     return (
-        <NavigationContainer>
-            <StatusBar hidden={true} />
-            <PaperProvider theme={theme}>
-                <Routes />
-            </PaperProvider>
-        </NavigationContainer>
+        <UserProvider>
+            <NavigationContainer>
+                <StatusBar hidden={true} />
+                <PaperProvider theme={theme}>
+                    <Routes />
+                </PaperProvider>
+            </NavigationContainer>
+        </UserProvider>
     );
 };
 
