6d1a8dd3af96ceb39ad2580c5a639e8220cf0e8c
@lucas.vuicik <lucas.fernandes@paipe.co>
2023-01-04 22:25:35 -0300

    feat: created a temporary theme to the project

---
+++ src/global/styles/theme.js
@@ -0,0 +1,13 @@
+export const colors = {
+  black: "#000",
+  blue: "#039be6",
+
+  grey800: '#373333',
+  grey700: '#504D4D',
+  grey600: '#696666',
+  grey300: '#B4B3B3',
+  grey200: '#CDCCCC',
+  grey100: "#FBFBFB",
+
+  white: "#fff",
+};
