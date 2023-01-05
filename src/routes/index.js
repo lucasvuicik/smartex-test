c571c6f3e2cf81c64f56c48ea5dd62ccfd6688c6
@lucas.vuicik <lucas.fernandes@paipe.co>
2023-01-05 12:09:26 -0300

    refactor: improved the layout of the drawer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,69 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
+import { Avatar, useTheme } from "react-native-paper";
+
 import { StackRoutes } from "./stackRoutes";
 import { CustomDrawer } from "../components";
 
 const Drawer = createDrawerNavigator();
 
 export const Routes = () => {
+  const { colors } = useTheme();
   return (
     <Drawer.Navigator
       drawerContent={(props) => <CustomDrawer {...props} />}
       screenOptions={{
         headerShown: false,
         drawerStyle: { backgroundColor: "#F1F1F1" },
-        drawerActiveBackgroundColor: "#F1F1F1",
-        drawerActiveTintColor: "orange",
-        drawerInactiveTintColor: "black",
+        drawerLabelStyle: { marginLeft: -20, fontSize: 16 },
+        drawerActiveBackgroundColor: colors.blue,
+        drawerActiveTintColor: colors.white,
+        drawerInactiveTintColor: colors.blue600,
       }}
     >
-      <Drawer.Screen name="To Do" component={StackRoutes} />
-      <Drawer.Screen name="Configuracoes" component={StackRoutes} />
-      <Drawer.Screen name="Sair" component={StackRoutes} />
+      <Drawer.Screen
+        name="Tarefas"
+        component={StackRoutes}
+        options={{
+          drawerIcon: () => (
+            <Avatar.Icon
+              size={24}
+              icon="check"
+              color={colors.white}
+              style={{ backgroundColor: colors.blue600 }}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Configuracoes"
+        component={StackRoutes}
+        options={{
+          drawerIcon: () => (
+            <Avatar.Icon
+              size={24}
+              icon="folder"
+              color={colors.white}
+              style={{ backgroundColor: colors.blue600 }}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Sair"
+        component={StackRoutes}
+        options={{
+          drawerIcon: () => (
+            <Avatar.Icon
+              size={24}
+              icon="folder"
+              color={colors.white}
+              style={{ backgroundColor: colors.blue600 }}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 };
